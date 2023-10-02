e4d3138495002432c0d80e8ee57da392a883780f
Chamindu Udakara <42435576+Chamindu36@users.noreply.github.com>
2023-10-03 00:15:32 +0530

    Update server.js to improve the code quality

--- server/src/server.js
+++ server/src/server.js
@@ -1,25 +1,42 @@
+// Importing required modules
 const http = require("http");
-
-require("dotenv").config();
-
+const dotenv = require("dotenv");
 const app = require("./app");
 const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
 const { loadLaunchData } = require('./models/launches.model');
 
+// Loading environment variables
+dotenv.config();
 
+// Defining the port to listen on
 const PORT = process.env.PORT || 8000;
 
+// Creating a server with the app
 const server = http.createServer(app);
 
+/**
+ * Function to start the server
+ */
 async function startServer() {
-  await mongoConnect();
-  await loadPlanetsData();
-  await loadLaunchData();
+  try {
+    // Connect to MongoDB
+    await mongoConnect();
 
-  server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`);
-  });
+    // Load planets data
+    await loadPlanetsData();
+
+    // Load launch data
+    await loadLaunchData();
+
+    // Start the server
+    server.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}...`);
+    });
+  } catch (error) {
+    console.error('Error starting the server:', error);
+  }
 }
 
+// Call the startServer function
 startServer();
